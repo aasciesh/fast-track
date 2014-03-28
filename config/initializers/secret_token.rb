8b306e993052e7ca982a8851e8f04b95cd58732c
Asish Bhattarai <asish.bhattarai@extremia.fi>
2014-03-28 09:31:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FastTrack::Application.config.secret_token = '192263a95d93a143b5505ad3b5de26ea8ed30c599f63fe10e210208126722850779f64599e96c6485449540010adfe717a693e016a8312507aaf75c8329647ed'
